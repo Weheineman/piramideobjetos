7e2385baa083962f082b3f4eaec84a74c492be24
Weheineman <gi4nn1@yahoo.de>
2015-07-04 15:56:26 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,32 +1,46 @@
 class Celda:
-  def __init__(self,h1,h2):
-    self.val=0
-    self.hijo1=h1
-    self.hijo2=h2
+	def __init__(self,h1,h2):
+		self.val=0
+		self.hijo1=h1
+		self.hijo2=h2
   
-  def Resolv(self):
-    flag=False
-    #Parte fea de fijarse
-    
-    flag=flag or hijo1.Resolv() or hijo2.Resolv()
-    return flag
-    
-  def Solved(self):
-    flag=true
-    if(self.val==0)
-      flag=false
-    flag=flag and hijo1.Solved() or hijo2.Solved()
-    
-  def setval(self, a)
-    self.val=a
+	def Resolv(self):
+		flag=False
+		#Parte fea de fijarse
+		if(self.val==0):
+			if( (self.hijo1.val!=0) and (self.hijo2.val!=0) ):
+				self.val=self.hijo1.val+self.hijo2.val
+				flag=True
+		else:
+			if( (self.hijo1.val==0) and (self.hijo2.val!=0) ):
+				self.hijo1.val=self.val-self.hijo2.val
+				flag=True
+			if( (self.hijo2.val==0) and (self.hijo1.val!=0) ):
+				self.hijo2.val=self.val-self.hijo1.val
+				flag=True
+		
+		flag=flag or self.hijo1.Resolv() or self.hijo2.Resolv()
+		return flag
+		
+	def Solved(self):
+		flag=True
+		if(self.val==0):
+			flag=False
+		flag=flag and self.hijo1.Solved() or self.hijo2.Solved()
+		
+	def setval(self, a):
+		self.val=a
 
 
 class CeldaAbajo(Celda):
-  def __init__(self)
-    self.valor=0
-    
-  def Resolv(self):
-    return false
+	def __init__(self):
+		self.val=0
+		
+	def Resolv(self):
+		return False
+
+	def Solved(self):
+  		return self.val==0
 
 
 #HACER PIRAMIDE A MANO
@@ -36,54 +50,74 @@ piso2=CeldaAbajo()
 piso3=CeldaAbajo()
 piso4=CeldaAbajo()
 piso5=CeldaAbajo()
-celda40=(piso0,piso1)
-celda41=(piso1,piso2)
-celda42=(piso2,piso3)
-celda43=(piso3,piso4)
-celda44=(piso4,piso5)
-celda30=(celda40,celda41)
-celda31=(celda41,celda42)
-celda32=(celda42,celda43)
-celda33=(celda43,celda44)
-celda20=(celda30,celda31)
-celda21=(celda31,celda32)
-celda22=(celda32,celda33)
-celda10=(celda20,celda21)
-celda11=(celda21,celda22)
-celda00=(celda10,celda11)
+celda40=Celda(piso0,piso1)
+celda41=Celda(piso1,piso2)
+celda42=Celda(piso2,piso3)
+celda43=Celda(piso3,piso4)
+celda44=Celda(piso4,piso5)
+celda30=Celda(celda40,celda41)
+celda31=Celda(celda41,celda42)
+celda32=Celda(celda42,celda43)
+celda33=Celda(celda43,celda44)
+celda20=Celda(celda30,celda31)
+celda21=Celda(celda31,celda32)
+celda22=Celda(celda32,celda33)
+celda10=Celda(celda20,celda21)
+celda11=Celda(celda21,celda22)
+celda00=Celda(celda10,celda11)
 
 
 #INPUT
-print ("Ingrese los valores de las celdas por fila desde arriba y desde la izquierda, ingrese 0 en caso de que la celda no tenga contenido:\n")
+print ("Ingrese los valores de las celdas por fila desde arriba y desde la izquierda, ingrese 0 en caso de que la celda no tenga contenido:")
+print("Ingrese la celda número 1 de la fila 1")
 celda00.setval(input())
+print("Ingrese la celda número 1 de la fila 2")
 celda10.setval(input())
+print("Ingrese la celda número 2 de la fila 2")
 celda11.setval(input())
+print("Ingrese la celda número 1 de la fila 3")
 celda20.setval(input())
+print("Ingrese la celda número 2 de la fila 3")
 celda21.setval(input())
+print("Ingrese la celda número 3 de la fila 3")
 celda22.setval(input())
+print("Ingrese la celda número 1 de la fila 4")
 celda30.setval(input())
+print("Ingrese la celda número 2 de la fila 4")
 celda31.setval(input())
+print("Ingrese la celda número 3 de la fila 4")
 celda32.setval(input())
+print("Ingrese la celda número 4 de la fila 4")
 celda33.setval(input())
+print("Ingrese la celda número 1 de la fila 5")
 celda40.setval(input())
+print("Ingrese la celda número 2 de la fila 5")
 celda41.setval(input())
+print("Ingrese la celda número 3 de la fila 5")
 celda42.setval(input())
+print("Ingrese la celda número 4 de la fila 5")
 celda43.setval(input())
+print("Ingrese la celda número 5 de la fila 5")
 celda44.setval(input())
+print("Ingrese la celda número 1 de la fila 6")
 piso0.setval(input())
+print("Ingrese la celda número 2 de la fila 6")
 piso1.setval(input())
+print("Ingrese la celda número 3 de la fila 6")
 piso2.setval(input())
+print("Ingrese la celda número 4 de la fila 6")
 piso3.setval(input())
+print("Ingrese la celda número 5 de la fila 6")
 piso4.setval(input())
+print("Ingrese la celda número 6 de la fila 6")
 piso5.setval(input())
 
 
 #PROCESO
-sinresolver=true
-while(sinresolver)
-  sinresolver=celda00
-  .Resolv()
-
+sinresolver=True
+while(sinresolver):
+  sinresolver=celda00.Resolv()
+  
 #OUTPUT
 print (celda00.val)
 print (str(celda10.val)+str(celda11.val))
