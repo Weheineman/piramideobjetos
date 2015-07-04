1140653b991c92dfa548099a2f887835ad2deda5
ivandlugo <ivandlugo@hotmail.com>
2015-07-03 21:06:02 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,33 @@
-sdfsdgfsdgv;
-sdfsdgvfds;
-#testing
+class Celda:
+  def __init__(self,h1,h2):
+    self.valor=0
+    self.hijo1=h1
+    self.hijo2=h2
+  
+  def Resolv(self):
+    flag=False
+    #Parte fea de fijarse
+    flag=flag or hijo1.Resolv() or hijo2.Resolv()
+    return flag
+
+
+class CeldaAbajo(Celda):
+  def __init__(self)
+    self.valor=0
+    
+  def Resolv(self):
+    return false
+
+
+#HACER PIRAMIDE A MANO
+
+
+#INPUT
+
+
+#PROCESO
+sinresolver=true
+while(sinresolver)
+  sinresolver=Cima.Resolv()
+
+#OUTPUT
